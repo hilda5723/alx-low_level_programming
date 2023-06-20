c9334b1270c723ab22b46f334ba7f569b81c6cef
hilda5723 <hildajemeli@gmail.com>
2023-06-20 11:04:26 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,16 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+ * print_alphabet - print the alphabet in lowercase, followed by a new line.
+ * Code by Prince Solomon
+*/
+
+void print_alphabet(void)
 {
-print_alphabet();
-return (0);
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	putchar('\n');
 }
